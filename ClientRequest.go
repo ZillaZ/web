58b0877dfacb04ffcfd62db5e139d4c5bd15cea3
Lucas Henrique <lucasmula125@gmail.com>
2024-10-10 21:53:55 -0300

    removed some things

--- ClientRequest.go
+++ ClientRequest.go
@@ -3,22 +3,10 @@ package web
 import (
 	"encoding/json"
 	"io"
-	"net/http"
 )
-
-type LoginRequest struct {
-	email    string
-	password string
-}
 
 func FromJson(data []byte, instance any) {
 	json.Unmarshal(data, instance)
-}
-
-func Login(request *http.Request) {
-	jsonData := ReadJson(request.Body)
-	instance := LoginRequest{}
-	FromJson(jsonData, &instance)
 }
 
 func ReadJson(reader io.ReadCloser) []byte {
